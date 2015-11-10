e630a115718196941f64ab5fafa2c6a685029988
Vadim Rutkovsky <vrutkovs@redhat.com>
2015-11-10 15:23:38 +0100

    Print out some more info

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-set -x
 set -e
 
 REPO=${REPO:-https://github.com/ManageIQ/manageiq}
 BRANCH=${BRANCH:-master}
 
-git clone --depth=1 -b $BRANCH $REPO
+echo "Repo: $REPO"
+echo "Branch: $BRANCH"
 
-# Installing
+
+echo "Installing ManageIQ"
 cd /manageiq
 bundle install --without qpid
 cp config/database.pg.yml config/database.yml
 cp certs/v2_key.dev certs/v2_key
 
-# Starting manageIQ
+echo "Installing Postgres and memcached"
 nohup /start_postgres.sh &
 nohup /usr/bin/memcached -u root &
 echo "waiting for the DB to start"
@@ -23,13 +24,12 @@ if [ -e /var/lib/pgsql/initialized ]
 then
 	echo "Reusing existing DB"
 else
-	echo "Init DB"
+	echo "Initialising DB"
 	sudo -u postgres sh /createDB.sh
 	rake db:migrate
 	touch /var/lib/pgsql/initialized
 fi
 
-# Make use of docker logs
+echo "Starting EVM"
 ln -sf /dev/stdout log/evm.log
-
 rake evm:start
